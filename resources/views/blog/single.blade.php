705ff799bcbccde0eb8709353a69bc99f6391f80
powolnymarcel <powolnymarcel@gmail.com>
2016-07-26 01:40:22 +0200

    continuation du projet

--- resources/views/blog/single.blade.php
+++ resources/views/blog/single.blade.php
@@ -8,6 +8,8 @@
         <div class="col-md-8">
             <h1>{{$post->title}}</h1>
             <p>{{$post->body}}</p>
+            <hr>
+            <p>Posté dans la catégorie : {{$post->category->name}}</p>
         </div>
                 </div>
     </div>
