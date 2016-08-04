7e8217878edc51d8049c3806de32427bdf654ab0
powolnymarcel <powolnymarcel@gmail.com>
2016-08-04 13:07:52 +0200

    continuation

--- database/migrations/2016_07_26_085724_create_tags_table.php
+++ database/migrations/2016_07_26_085724_create_tags_table.php
@@ -14,6 +14,8 @@ class CreateTagsTable extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+
             $table->timestamps();
         });
     }
